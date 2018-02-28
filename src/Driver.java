bd5c75c148318d00fbc72a6b50d8e381e579b10a
Gomeyy <mgomes163@gmail.com>
2018-02-27 20:46:31 -0500

    //Separated Students //Added course 2

--- src/Driver.java
+++ src/Driver.java
@@ -1,46 +1,26 @@
-import Restricted.Course;
+import res.Course;
 import main.College_Student;
 
 public class Driver {
 
     public static void main(String[] args) {
-        College_Student[] cS1 = new College_Student[]{
-                new College_Student("bobo",
-                        14,
-                        "9019901",
-                        true,
-                        "010192",
-                        4.0,
-                        "math",
-                        "A+"),
-                new College_Student("bobo",
-                        14,
-                        "9019901",
-                        true,
-                        "010192",
-                        4.0,
-                        "math",
-                        "A+"),
-                new College_Student("bobo",
-                        14,
-                        "9019901",
-                        true,
-                        "010192",
-                        4.0,
-                        "math",
-                        "A+"),
-                new College_Student("bobo",
-                        14,
-                        "9019901",
-                        true,
-                        "010192",
-                        4.0,
-                        "math",
-                        "A+")
-                };
+                College_Student s1 = new College_Student(
+                        "George", 19, "928937803", true,
+                        "9820198", 3.8, "Computer Engineering", "A-");
+                College_Student s2 = new College_Student(
+                        "Joseph", 21, "189945729", true,
+                        "5879018", 3.4, "Electrical Engineering", "B+");
+                College_Student s3 = new College_Student(
+                        "Destiny", 20, "6859283749", true,
+                        "6879109", 2.7, "Undecided", "C+");
+                College_Student s4 = new College_Student(
+                        "Brittney", 22, "192803945", true,
+                        "69092883", 3.5, "Undecided", "B-");
 
-        Course c1 = new Course(cS1, "Math");
-        System.out.println(c1.getCourse_name());
+        Course c1 = new Course(new College_Student[]{s1, s2}, "Computer Programming");
+        Course c2 = new Course(new College_Student[]{s3, s4}, "Physics 2");
+        System.out.println(c1.toString());
+        System.out.println(c2.toString());
     }
 
     }
